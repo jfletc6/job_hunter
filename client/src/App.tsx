a7c607104983788e803ebbcc092dac9815c91cc8
jfletc6 <joseph_fletcher4@yahoo.com>
2026-06-20 13:44:32 -0400

    Fix missing Navigate import in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import AddCompany from './pages/AddCompany'
