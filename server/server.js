be0155b9fc099519ab2b47e2afd1bfeb64c97505
jfletc6 <joseph_fletcher4@yahoo.com>
2026-06-19 22:06:06 -0400

    Restructure for Vercel deployment: add api/index.js, update vercel.json

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import companyRouter from './routes/companies.js'
 import userRouter from './routes/users.js'
 import authRouter from './routes/auth.js'
+import companyRouter from './routes/companies.js'
 
 dotenv.config()
 
@@ -14,21 +14,16 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = process.env.MONGO_URI
-mongoose.connect(uri, { dbName: "job-hunter" })
-  .then(() => {
-    console.log("Successfully connected to MongoDB via Mongoose!")
-  })
-  .catch((err) => {
-    console.error(err)
-  });
+mongoose.connect(process.env.MONGO_URI, { dbName: "job-hunter" })
+  .then(() => console.log("Successfully connected to MongoDB via Mongoose!"))
+  .catch((err) => console.error(err))
 
-app.use('/api/users', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/companies', companyRouter);
+app.use('/api/auth', authRouter)
+app.use('/api/users', userRouter)
+app.use('/api/companies', companyRouter)
 
-app.get('/', (req, res) => {
-  res.send('Job Hunter API is running')
-})
+app.get('/', (req, res) => res.send('Job Hunter API is running'))
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+export default app
